c78b8e2a46408e3f6db068a0c769fecc592b511b
nk1121 <nk1640r@gre.ac.uk>
2023-02-28 12:51:38 +0000

    courses details added

--- Student_Management_System/Student_Management_System/Form1.Designer.cs
+++ Student_Management_System/Student_Management_System/Form1.Designer.cs
@@ -28,13 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Adobe Arabic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(30, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(490, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "COMP1121_Computer_Architecture";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(408, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ELEC1412";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(30, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(408, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "COMP1213";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(30, 331);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(408, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "MATH1611";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(30, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 52);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "COURSES";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(30, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(408, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ELEE1126";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(682, 395);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(83, 43);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "LOG OUT";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Baskerville Old Face", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(30, 133);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(722, 57);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "ELEE1211_Computer_Architecture\r\n";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
     }
 }
 
