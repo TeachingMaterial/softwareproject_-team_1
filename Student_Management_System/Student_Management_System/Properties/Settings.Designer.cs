598beb1642b11596d0a5501db6e8e698cb836165
nirmalphilips2003 <114992268+nirmalphilips2003@users.noreply.github.com>
2023-03-22 13:38:03 +0000

    Revert "Phase/3 kb9488"

--- Student_Management_System/Student_Management_System/Properties/Settings.Designer.cs
+++ Student_Management_System/Student_Management_System/Properties/Settings.Designer.cs
@@ -8,29 +8,22 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Student_Management_System.Properties {
-    
-    
+namespace Student_Management_System.Properties
+{
+
+
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
-        
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
+    {
+
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-        
-        public static Settings Default {
-            get {
+
+        public static Settings Default
+        {
+            get
+            {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\karim\\Documents\\Uni Work\\" +
-            "Software Engineering\\students11.accdb\"")]
-        public string students11ConnectionString {
-            get {
-                return ((string)(this["students11ConnectionString"]));
             }
         }
     }
