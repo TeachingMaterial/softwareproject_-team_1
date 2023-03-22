47d56fc011fe499d8b527445b49b46e9efd4afd5
nk1121 <nk1640r@gre.ac.uk>
2023-03-22 17:57:17 +0000

    attendance page updated again due to merge issues

--- Student_Management_System/Student_Management_System/attendance.cs
+++ Student_Management_System/Student_Management_System/attendance.cs
@@ -16,5 +16,10 @@ namespace Student_Management_System
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
